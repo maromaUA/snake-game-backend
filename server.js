7c8aa4c6d1faebc19d3a7a7e9280329ef71574c3
Roman Marchuk <maroma1991@gmail.com>
2024-06-10 19:11:40 +0100

    env fix

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const { DB_HOST } = process.env;
+//const { DB_HOST } = process.env;
 mongoose
-  .connect(DB_HOST)
+  .connect("mongodb+srv://maroma1991:QAZ123wsx@cluster0.askrqto.mongodb.net/")
   .then(() => {
     app.listen(3001, () => {
       console.log("Server running. Use our API on port: 3001");
