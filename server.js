45953db3b06dc90c7a207749a5d3746dc358466a
Roman Marchuk <maroma1991@gmail.com>
2024-06-11 21:24:52 +0100

    port fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { DB_HOST } = process.env;
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(443, () => {
+    app.listen(3001, () => {
       console.log("Server running. Use our API on port: 443");
     });
     console.log("Database connection successful");
